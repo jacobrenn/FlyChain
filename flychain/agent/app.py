1dd60276a3eb12856e0b9170b5658dbb0bdc781e
Ian Sotnek <iansotnek@mylaptop-2.local>
2023-06-28 17:21:32 -0400

    feedback working now
    Signed-off-by: Ian Sotnek <iansotnek@mylaptop-2.local>

--- flychain/agent/app.py
+++ flychain/agent/app.py
@@ -1,4 +1,6 @@
+#import trubrics
 from trubrics.integrations.streamlit import FeedbackCollector
+import json
 import streamlit as st
 from streamlit_chat import message
 from streamlit_extras.colored_header import colored_header
@@ -24,12 +26,15 @@ if 'user_input' not in st.session_state:
 response_container = st.container()
 colored_header(label='', description='', color_name='blue-30')
 input_container = st.container()
+
 # Configure user feedback
+collector = FeedbackCollector(
+    component_name = 'default',
+    email = None,
+    password = None
+    )
 
-#collector = FeedbackCollector()
-
-
-#feedback.dict() if feedback else None
+user_feedback_path = '../user_feedback/feedback.json'
 
 def get_input():
     input_text = st.text_input("You: ", "", key="input")
@@ -50,12 +55,33 @@ with response_container:
         response = generate_response(user_input)
         st.session_state.user_input.append(user_input)
         st.session_state.bot_response.append(response)
-#        feedback = collector.st_feedback(
-#            feedback_type = 'thumbs',
-#            path = 'feedback.json'
-#        )
+
 
     if st.session_state['bot_response']:
         for i in range(len(st.session_state['bot_response'])):
             message(st.session_state['user_input'][i], is_user=True, key=str(i) + '_user')
             message(st.session_state['bot_response'][i], key=str(i))
+            feedback = collector.st_feedback(
+                feedback_type = 'thumbs',
+                model = 'langchain',
+                open_feedback_label="[Optional] Provide additional feedback",
+                save_to_trubrics = False
+            )
+
+            if feedback:
+                # Add 'bot_response' to 'feedback'
+                feedback['bot_response'] = st.session_state['bot_response'][i]
+
+                # Load the existing data
+                try:
+                    with open(user_feedback_path, 'r') as file:
+                        data = json.load(file)
+                except json.JSONDecodeError:  # if the file is empty, initialize 'data' as an empty list
+                    data = []
+
+                # Append the new feedback with the bot's response
+                data.append(feedback)
+
+                # Write the new data
+                with open(user_feedback_path, 'w') as file:
+                    json.dump(data, file, indent=4, default=str)
