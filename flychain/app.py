2a73a64ca9cb6450ac4b5c62f450fa56d1a3b495
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2023-06-28 16:04:12 -0700

    updating
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- flychain/app.py
+++ flychain/app.py
@@ -1,95 +1,24 @@
 #import trubrics
-from trubrics.integrations.streamlit import FeedbackCollector
-import json
-import streamlit as st
-from streamlit_chat import message
-from streamlit_extras.colored_header import colored_header
-from streamlit_extras.add_vertical_space import add_vertical_space
 from agent import create_agent
-import os
-
 
 agent = create_agent(model = 'gpt-4', chat_history = True)
 
-st.set_page_config(page_title = 'Flychain')
 
-# Generate empty lists for bot_response and user_input. -> add HTML for interactive feedback
-## bot_response stores AI generated responses
-if 'bot_response' not in st.session_state:
-    st.session_state['bot_response'] = ['How can I help you?']
-## user_input stores User's questions
-if 'user_input' not in st.session_state:
-    st.session_state['user_input'] = ['Hi!']
+import gradio as gr
+import random
+import time
 
-# Layout of input/response containers
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox()
+    clear = gr.ClearButton([msg, chatbot])
 
-response_container = st.container()
-colored_header(label='', description='', color_name='blue-30')
-input_container = st.container()
+    def respond(message, chat_history):
+        bot_message = agent.run(message)
+        chat_history.append((message, bot_message))
+        time.sleep(2)
+        return "", chat_history
 
-# Configure user feedback
-collector = FeedbackCollector(
-    component_name = 'default',
-    email = None,
-    password = None
-    )
+    msg.submit(respond, [msg, chatbot], [msg, chatbot])
 
-user_feedback_path = '../user_feedback/feedback.json'
-if not os.path.exists(os.path.dirname(user_feedback_path)):
-    os.makedirs(os.path.dirname(user_feedback_path))
-
-def get_input():
-    input_text = st.text_input("You: ", "", key="input")
-    return input_text
-## Applying the user input box
-with input_container:
-    user_input = get_input()
-
-# Response output #
-## Function for taking user prompt as input followed by producing AI generated responses
-def generate_response(prompt):
-    try:
-        response = agent.run(input = prompt)
-    except:
-        response = agent(prompt)
-
-    return response
-
-## Conditional display of AI generated responses as a function of user provided prompts
-with response_container:
-    if user_input:
-        response = generate_response(user_input)
-        st.session_state.user_input.append(user_input)
-        st.session_state.bot_response.append(response)
-
-
-    if st.session_state['bot_response']:
-        for i in range(len(st.session_state['bot_response'])):
-            message(st.session_state['user_input'][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state['bot_response'][i], key=str(i))
-            feedback = collector.st_feedback(
-                feedback_type = 'thumbs',
-                model = 'langchain',
-                open_feedback_label="[Optional] Provide additional feedback",
-                save_to_trubrics = False,
-                key = str(i)
-            )
-
-            if feedback:
-                # Add 'bot_response' to 'feedback'
-                feedback['bot_response'] = st.session_state['bot_response'][i]
-                feedback['user_input'] = st.session_state['user_input'][i]
-
-                # Load the existing data
-                try:
-                    with open(user_feedback_path, 'r') as file:
-                        data = json.load(file)
-                except FileNotFoundError:  # if the file is empty, initialize 'data' as an empty list
-                    data = []
-
-                # Append the new feedback with the bot's response
-                data.append(feedback)
-
-                # Write the new data
-                with open(user_feedback_path, 'w') as file:
-                    json.dump(data, file, indent=4, default=str)
+demo.launch()
