87f4c8fe9770d5a09ca85defd844da9b8615a861
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2023-06-29 08:18:32 -0700

    updating
    Signed-off-by: Jacob Renn <77127228+jacobrenn@users.noreply.github.com>

--- flychain/app.py
+++ flychain/app.py
@@ -6,8 +6,12 @@ import click
 @click.command()
 @click.option('--model', '-m', default = 'gpt-4')
 @click.option('--history/--no-history', default = True)
-def deploy(model, history):
-    agent = create_agent(model = model, chat_history = history)
+@click.option('--temperature', type = int, default = 0)
+@click.option('--task', type = str, default = 'text-generation')
+@click.option('--huggingface-pipeline-kwargs', type = dict, default = None)
+@click.option('--huggingface-model-kwargs', type = dict, default = None)
+def deploy(model, history, temperature, task, huggingface_pipeline_kwargs, huggingface_model_kwargs):
+    agent = create_agent(model = model, chat_history = history, temperature = temperature, task = task, huggingface_pipeline_kwargs = huggingface_pipeline_kwargs, huggingface_model_kwargs = huggingface_model_kwargs)
 
     with gr.Blocks() as demo:
         chatbot = gr.Chatbot()
